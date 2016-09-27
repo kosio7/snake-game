3f8dd172178c24c440b56c88d3d9b1a285b38ef2
Konstantin Stefanov <kosio7@gmail.com>
2016-09-27 14:28:10 +0300

    Snake game. Initial commit.

---
+++ public_html/JS/snake.js
@@ -0,0 +1,16 @@
+let snake;
+let context;
+let screenWidth;
+let screenHeight;
+
+function gameInit() {
+    let canvas = document.getElementById("screen-view");
+}
+
+function gameLoop() {
+    
+}
+
+function gameDraw() {
+    
+}
